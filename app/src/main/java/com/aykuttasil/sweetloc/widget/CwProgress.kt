4872a298101b4d9b4801fc38ba1e003ba0afd4a0
Aykut Asil <aykuttasil@gmail.com>
2019-05-05 23:49:51 +0300

    Update CwProgress.kt

--- app/src/main/java/com/aykuttasil/sweetloc/widget/CwProgress.kt
+++ app/src/main/java/com/aykuttasil/sweetloc/widget/CwProgress.kt
@@ -1,28 +1,26 @@
 package com.aykuttasil.sweetloc.widget
 
 import android.content.Context
-import android.graphics.Canvas
 import android.util.AttributeSet
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.aykuttasil.sweetloc.R
 
 
-class CwProgress @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : ConstraintLayout(context, attrs, defStyleAttr) {
+class CwProgress @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    LinearLayout(context, attrs, defStyleAttr) {
+
+
+    private val progressBar: ProgressBar
+    private val textView: TextView
 
     init {
-        inflate(context, R.layout.progress_dialog_fragment_layout, this)
-        val progressBar: ProgressBar = findViewById(R.id.progressBar)
-        val textView: TextView = findViewById(R.id.textView)
+        val vi = inflate(context, R.layout.progress_dialog_fragment_layout, this)
+
+        progressBar = vi.findViewById(R.id.progressBar)
+        textView = vi.findViewById(R.id.textView)
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-    }
 
 }
