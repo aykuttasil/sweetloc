71e97c1189c02f84aaf3a0af29f57ddd5a1b589b
Aykut Asil <aykuttasil@gmail.com>
2019-05-08 23:56:15 +0300

    Create DebuggingHelper.kt

---
+++ app/src/main/java/com/aykuttasil/sweetloc/helper/DebuggingHelper.kt
@@ -0,0 +1,17 @@
+package com.aykuttasil.sweetloc.helper
+
+
+object DebuggingHelper {
+
+    internal fun printObject(obj: Any) {
+        for (field in obj.javaClass.declaredFields) {
+            val name = field.name
+            try {
+                val value = field.get(obj)
+                System.out.printf("Field name: %s, Field value: %s%n", name, value)
+            } catch (t: Throwable) {
+            }
+
+        }
+    }
+}
