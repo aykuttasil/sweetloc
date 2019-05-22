940e95b42c510fd131571cadac6b9c180dc7b2fc
Aykut Asil <aykuttasil@gmail.com>
2019-05-23 01:01:09 +0300

    update gradle and log refactoring

--- app/src/main/java/com/aykuttasil/sweetloc/App.kt
+++ app/src/main/java/com/aykuttasil/sweetloc/App.kt
@@ -11,15 +11,18 @@ import com.aykuttasil.sweetloc.service.NotificationReceivedHandler
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
 import com.onesignal.OneSignal
-import com.orhanobut.logger.LogLevel
+import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import com.vondear.rxtool.RxTool
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import dagger.android.HasServiceInjector
 import io.fabric.sdk.android.Fabric
+import timber.log.Timber
 import javax.inject.Inject
+
 
 open class App : Application(), HasActivityInjector, HasServiceInjector {
 
@@ -29,11 +32,6 @@ open class App : Application(), HasActivityInjector, HasServiceInjector {
     @Inject
     lateinit var activityDispatchingServiceInjector: DispatchingAndroidInjector<Service>
 
-    override fun onCreate() {
-        super.onCreate()
-        AppInjector.init(this)
-        initSweetLoc()
-    }
 
     override fun activityInjector(): DispatchingAndroidInjector<Activity> {
         return activityDispatchingAndroidInjector
@@ -43,14 +41,24 @@ open class App : Application(), HasActivityInjector, HasServiceInjector {
         return activityDispatchingServiceInjector
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        AppInjector.init(this)
+        initSweetLoc()
+    }
+
     private fun initSweetLoc() {
-        Logger.init("SweetlocLogger")
-            .methodCount(3)
-            .logLevel(if (BuildConfig.DEBUG) LogLevel.FULL else LogLevel.NONE)
-            .methodOffset(0)
+        initLog()
+        initFabric()
+        initOneSignal()
 
-        initializeFabric()
+        // FacebookSdk.sdkInitialize(applicationContext)
+        // AppEventsLogger.activateApp(this)
 
+        RxTool.init(this)
+    }
+
+    private fun initOneSignal() {
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.ERROR)
         // OneSignal.init(this, "535821025252", "283c0725-f1ae-434a-8ea5-09f61b1246fc", new NotificationOpenedHandler(), new NotificationReceivedHandler());
 
@@ -59,14 +67,32 @@ open class App : Application(), HasActivityInjector, HasServiceInjector {
             .setNotificationOpenedHandler(NotificationOpenedHandler())
             //.autoPromptLocation(true)
             .init()
-
-        // FacebookSdk.sdkInitialize(applicationContext)
-        // AppEventsLogger.activateApp(this)
-
-        RxTool.init(this)
     }
 
-    private fun initializeFabric() {
+    private fun initLog() {
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(true)  // (Optional) Whether to show thread info or not. Default true
+            .methodCount(2)         // (Optional) How many method line to show. Default 2
+            .methodOffset(5)        // (Optional) Hides internal method calls up to offset. Default 5
+            // .logStrategy(customLog)      // (Optional) Changes the log strategy to print out. Default LogCat
+            .tag("SweetlocLogger")  // (Optional) Global tag for every log. Default PRETTY_LOGGER
+            .build()
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
+
+        // Timber.plant(Timber.DebugTree())
+        // Set methodOffset to 5 in order to hide internal method calls
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                Logger.log(priority, tag, message, t)
+            }
+        })
+    }
+
+    private fun initFabric() {
         val crashlyticsCore = CrashlyticsCore.Builder()
             .disabled(BuildConfig.DEBUG)
             .build()
