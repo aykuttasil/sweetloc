259b3e2c42b8c1c787772d859cac048261937b99
Aykut Asil <aykuttasil@gmail.com>
2019-05-23 01:04:02 +0300

    updates

--- app/src/main/java/com/aykuttasil/sweetloc/ui/fragment/roommemberlist/RoomMemberListViewModel.kt
+++ app/src/main/java/com/aykuttasil/sweetloc/ui/fragment/roommemberlist/RoomMemberListViewModel.kt
@@ -1,8 +1,8 @@
 package com.aykuttasil.sweetloc.ui.fragment.roommemberlist
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.aykuttasil.sweetloc.App
 import com.aykuttasil.sweetloc.data.UserModel
 import com.aykuttasil.sweetloc.data.repository.RoomRepository
 import com.aykuttasil.sweetloc.ui.BaseAndroidViewModel
@@ -10,27 +10,29 @@ import io.reactivex.rxkotlin.addTo
 import javax.inject.Inject
 
 class RoomMemberListViewModel @Inject constructor(
-    private val app: App,
+    private val app: Application,
     private val roomRepository: RoomRepository
 ) : BaseAndroidViewModel(app) {
 
     val liveRoomMemberList: MutableLiveData<List<UserModel>> = MutableLiveData()
 
     fun getRoomMemberList(roomId: String): LiveData<List<UserModel>> {
-        roomRepository.getRoomMembers(roomId).subscribe({
-            liveRoomMemberList.postValue(it)
-        }, {
-            it.printStackTrace()
-        }).addTo(disposables)
+        roomRepository.getRoomMembers(roomId)
+            .subscribe({
+                liveRoomMemberList.postValue(it)
+            }, {
+                it.printStackTrace()
+            }).addTo(disposables)
 
         return liveRoomMemberList
     }
 
     fun setRoomMemberList(roomId: String) {
-        roomRepository.getRoomMembers(roomId).subscribe({
-            liveRoomMemberList.postValue(it)
-        }, {
-            it.printStackTrace()
-        }).addTo(disposables)
+        roomRepository.getRoomMembers(roomId)
+            .subscribe({
+                liveRoomMemberList.postValue(it)
+            }, {
+                it.printStackTrace()
+            }).addTo(disposables)
     }
 }
