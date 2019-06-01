eb9890e7828d0031145d33efa854510c0fce0aef
Aykut Asil <aykuttasil@gmail.com>
2019-06-01 14:40:22 +0300

    Update Models.kt

--- app/src/main/java/com/aykuttasil/sweetloc/data/Models.kt
+++ app/src/main/java/com/aykuttasil/sweetloc/data/Models.kt
@@ -1,5 +1,7 @@
 package com.aykuttasil.sweetloc.data
 
+import android.location.Location
+import com.aykuttasil.androidbasichelperlib.SuperHelper
 import com.aykuttasil.sweetloc.data.local.entity.UserEntity
 import java.text.DateFormat
 import java.util.Date
@@ -15,9 +17,9 @@ data class UserModel(
     var userToken: String? = null,
     var userImageUrl: String? = null,
     var userOneSignalId: String? = null,
-    var userCity: String? = null,
-    var userCreateDate: Long? = Date().time,
-    var userCreateDateString: String? = DateFormat.getInstance().format(Date())
+    var userCity: String? = null
+    //var userCreateDate: Long? = Date().time,
+    //var userCreateDateString: String? = DateFormat.getInstance().format(Date())
 )
 
 data class RoomEntity(
@@ -43,19 +45,49 @@ data class LocationEntity(
 )
 
 data class RoomLocationModel(
-    var user: UserEntity,
-    var location: LocationEntity
+    var user: UserEntity? = null,
+    var location: LocationEntity? = null
 )
+
+data class RoomMemberLocationModel(
+    var user: UserModel? = null,
+    var location: LocationEntity? = null
+)
+
+fun RoomEntity.toRoomMemberLocationModel(userModel: UserModel, location: LocationEntity): RoomMemberLocationModel {
+    return RoomMemberLocationModel(userModel, location)
+}
+
+fun Location.toLocationEntity(): LocationEntity {
+    return LocationEntity(
+        latitude = this.latitude,
+        longitude = this.longitude,
+        accuracy = this.accuracy,
+        address = this.provider,
+        time = this.time,
+        formatTime = SuperHelper.getFormatTime(this.time),
+        createDate = SuperHelper.formatTime
+    )
+}
 
 fun roomsNode() = "rooms"
 fun roomNode(roomId: String) = "${roomsNode()}/$roomId"
+
 fun roomMembersNode(roomId: String) = "${roomNode(roomId)}/members"
 fun roomMemberNode(roomId: String, userId: String) = "${roomMembersNode(roomId)}/$userId"
+
 fun roomLocationsNode(roomId: String) = "${roomNode(roomId)}/locations"
+
+fun roomMembersLocationNode(roomId: String) = "${roomMembersNode(roomId)}/location"
+fun roomMemberLocationNode(roomId: String, userId: String) = "${roomMembersNode(roomId)}/$userId"
+
+/*----*/
 
 fun usersNode() = "users"
 fun userNode(userId: String) = "${usersNode()}/$userId"
+
 fun userRoomsNode(userId: String) = "${userNode(userId)}/rooms"
 fun userRoomNode(userId: String, roomId: String) = "${userRoomsNode(userId)}/$roomId"
+
 fun userLocationsNode(userId: String) = "${userNode(userId)}/locations"
 
