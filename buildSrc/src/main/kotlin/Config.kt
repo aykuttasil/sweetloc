a9455cc58c41e582b732dd367a409aef691824f6
Aykut Asil <aykuttasil@gmail.com>
2019-06-26 01:53:16 +0300

    common gradle files added

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -6,4 +6,7 @@ object Config {
     val targetSdk = 28
     val javaVersion = JavaVersion.VERSION_1_8
     val buildTools = "28.0.3"
+
+    const val versionCode = 1
+    const val versionName = "1.0.0"
 }
